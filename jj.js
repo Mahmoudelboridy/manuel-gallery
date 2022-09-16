c98ce129d492aa650e4f2b7e02515bd6e6c3c2d3
Mahmoudelboridy <111857983+Mahmoudelboridy@users.noreply.github.com>
2022-09-16 11:09:42 +0200

    Add files via upload

--- jj.js
+++ jj.js
@@ -1,6 +1,6 @@
 var img=['0.jpg','1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpeg'];
-let x=0;
-//let y=img.length-1;
+let x=3;
+let y=img.length-1;
 
 function mnr() {
 x=x+1;
@@ -8,17 +8,12 @@ document.im.src=img[x];
 if(x==img.length-1){
 x=-1;
 }
-return x;
 }
-y=mnr();
+
 function mntr() {
 y=y-1;
 document.im.src=img[y];
 if(y==0){
 y=img.length;
 }
-
-
 }
-
-
